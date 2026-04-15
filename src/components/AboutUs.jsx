f93635d29d0dd75cd1f3dfd061158d778311e5cd
Savri Joshi <savrirjoshi@gmail.com>
2026-04-15 12:55:51 +0530

    Add AboutUs component for nursery information

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is a plant shop offering a wide variety of house plants
+        to bring freshness and greenery into your home.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
